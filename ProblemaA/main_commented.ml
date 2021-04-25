169a3912e926b49076828048d460e1d1b8bd440c
monteiro141 <monteiro141@hotmail.com>
2021-04-25 19:59:26 +0100

    commented

--- ProblemaA/main_commented.ml
+++ ProblemaA/main_commented.ml
@@ -8,13 +8,30 @@ let rec fortytwo i counting=
     if(i >= 42) then(
       if(i = 42) then (0) else 
       (
-        let a = if ((i mod 2) = 0) then (1 + (fortytwo (i/2) counting)) else (counting)
+        let a = 
+          if ((i mod 2) = 0) then 
+            (1 + (fortytwo (i/2) counting)) 
+          else 
+            (counting)
         and
-        b = if (((i mod 3 = 0) || (i mod 4 = 0)) && (i mod 10) <> 0 && ((i/10) mod 10) <> 0) then ( 1 + (fortytwo (i - ((i mod 10) * ((i/10) mod 10))) counting)) else (counting)
+        b = 
+          if (((i mod 3 = 0) || (i mod 4 = 0)) && (i mod 10) <> 0 && ((i/10) mod 10) <> 0) then 
+            ( 1 + (fortytwo (i - ((i mod 10) * ((i/10) mod 10))) counting)) 
+          else 
+            (counting)
         and
-        c = if ((i mod 5 = 0)) then ( 1 + (fortytwo(i-42) counting)) else (counting)
+        c = 
+          if ((i mod 5 = 0)) then 
+            ( 1 + (fortytwo(i-42) counting)) 
+          else 
+            (counting)
         in
-        if(a <= b && a <= c) then (a)else if (b <= a && b <= c) then (b)else (c)
+          if(a <= b && a <= c) then 
+            (a)
+          else if (b <= a && b <= c) then 
+            (b)
+          else 
+            (c)
       )  
     )else(
       counting
